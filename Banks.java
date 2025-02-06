8d05aa91585b77bc50b86bd2064e5856036e613b
Divraj-M <138122681+Divraj-M@users.noreply.github.com>
2025-02-06 16:25:56 +0530

    Add files via upload

---
+++ Banks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Banks {
+	public static void main(String args[])
+	{
+		String name;
+		int age;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the name:");
+		name=s.next();
+		System.out.println("enter the age:");
+		age=s.nextInt();
+	}
+	
+
+}
