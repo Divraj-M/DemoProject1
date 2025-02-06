dc7addd114c043918358f8b7fbf2fd3855c3be8d
Divraj-M <138122681+Divraj-M@users.noreply.github.com>
2025-02-06 16:35:01 +0530

    Update Banks.java

--- Banks.java
+++ Banks.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class Banks {
 	public static void main(String args[])
 	{
-		String name;
-		int age;
-		Scanner s=new Scanner(System.in);
-		System.out.println("enter the name:");
-		name=s.next();
-		System.out.println("enter the age:");
-		age=s.nextInt();
+		if(args.length<2)
+		{
+			System.out.println("Usage : java parameter <name> <age>");
+			return;
+		}
+		String name=args[0];
+		int age=Integer.parseInt(args[1]);
 	}
 	
 
